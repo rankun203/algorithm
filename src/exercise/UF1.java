6630e1814ebffa92300b0ad5549d4e90b00abd2b
Lov Why <rankun203@gmail.com>
2013-06-21 11:33:56 +0800

    添加algs.jar 提交UF1.java 把自己编译的algs.jar类库加进来（该类库将所有的类放进了algs4包中，以便在eclipse工程中引用） 提交UF1.java：union-find的Quick-Union算法的实现。

--- src/exercise/UF1.java
+++ src/exercise/UF1.java
@@ -5,7 +5,7 @@ import algs4.In;
 
 /**
  * 动态连通性问题,基于<b>quick-union</b>算法<br>
- * 每个触点所对应的id[]元素都是
+ * 每个触点所对应的id[]元素都是同一个分量中的另一个触点的名称（也可能是它自己）———链接
  * @author mindfine
  */
 public class UF1 {
@@ -33,24 +33,34 @@ public class UF1 {
 		return (find(q) == find(p));
 	}
 	/**
-	 * 查找指定触点 p 的连通分量
+	 * 查找指定触点 p 的连通分量<br>
+	 * 从一个节点得到该节点id[p]对应的节点，然后让p等于那个节点，<br>
+	 * 再次检查那个节点（已经由p来指向）的id[p]对应的节点，然后再顺着链接找下去。
 	 * @param p 指定触点 p
 	 * @return 触点 p 的连通分量索引
 	 */
 	public int find(int p) {
-		return id[p];
+		while(p != id[p])	p = id[p];
+		return p;
 	}
 	/**
 	 * 将 p 和 q 归到同一个分量中。
+	 * <ol>
+			<li>找到要归并到一起的两个分量的根节点</li>
+			<li>如果他们的根节点相同则什么都不做</li>
+			<li>把一个分量的根节点指向另一个分量的根节点，从而让这两个分量归为一个分量</li>
+			<li>把分量总数减一 (二分量合一)</li>
+		</ol><br>
 	 * @param p 触点 p
 	 * @param q 触点 q
 	 */
 	public void union(int p, int q){
-		int pId = id[p];
-		int qId = id[q];
-		if(pId == qId)	return;
-		for(int i = 0; i < id.length; i++)
-			if(id[i] == pId)	id[i] = qId;
+		int qRoot = find(q);
+		int pRoot = find(p);
+		if(qRoot == pRoot)	return;
+
+		id[qRoot] = id[pRoot];
+		
 		count--;
 	}
 	/**
@@ -65,7 +75,7 @@ public class UF1 {
 	 * 测试用例，输入一个 .txt文件，解析其中的所有触点，并建立连通分量。
 	 */
 	public static void main(String[] args) {
-		In in = new In("tinyUF.txt");
+		In in = new In("largeUF.txt");
 		int [] ints = in.readAllInts();
 		int N = ints[0];
 		UF1 uf = new UF1(N);
@@ -75,7 +85,7 @@ Timer t = new Timer();//计时开
 			int q = ints[++i];
 			if(uf.connected(p, q))	continue;
 			uf.union(p, q);
-			System.out.println(p + " " + q);
+System.out.println(p + " " + q);
 		}
 System.err.println("********************************************************* - " + t.round() + "ms");
 			In in1 = new In("mediumUF.txt");
@@ -88,7 +98,7 @@ t.round();//计时开始
 				int q = ints1[++i];
 				if(uf1.connected(p, q))	continue;
 				uf1.union(p, q);
-				System.out.println(p + " " + q);
+System.out.println(p + " " + q);
 			}
 System.err.println("********************************************************* - " + t.round() + "ms");
 	}
